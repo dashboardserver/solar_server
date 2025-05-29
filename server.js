bb59575e354350bfdce317ddc8114042a1aee3fe
PunpunSuphanut <qpunpunn@gmail.com>
2025-05-29 10:21:38 +0700

    Edit: server.js add app.get

--- server.js
+++ server.js
@@ -12,9 +12,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Backend is running');
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
+
 
 app.use('/api/auth', authRoutes);
 app.use('/api/seafdec', seafdecRoutes);
